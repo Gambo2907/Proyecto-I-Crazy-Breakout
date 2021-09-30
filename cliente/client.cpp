ccbfc295bdebc3fe92193c6f16e8c55ea2eb2ebe
gambo2907 <sgamo2220@gmail.com>
2021-09-30 17:24:23 -0600

    Client

--- cliente/client.cpp
+++ cliente/client.cpp
@@ -1,16 +1,34 @@
+#include <iostream>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <string>
 #define PORT 8080
+
+using namespace std;
+
+/*
+g++ -o client client.cpp
+*/
    
 int main(int argc, char const *argv[])
 {
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
-    char *hello = "Attempting connection";
+    const char *hello = "Attempting connection";
     char buffer[1024] = {0};
+
+    std::string ipAddress;
+    int port;
+    cout<< "Please enter the ip address you wish to connect to: ";
+    cin>>ipAddress;
+    cout<<"Please enter the port you wish to connect to: ";
+    cin>>port;
+
+    const char *address = ipAddress.c_str();
+
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Socket creation error \n");
@@ -18,10 +36,10 @@ int main(int argc, char const *argv[])
     }
    
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_port = htons(port);
        
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+    if(inet_pton(AF_INET, address, &serv_addr.sin_addr)<=0) 
     {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
